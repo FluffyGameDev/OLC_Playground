d83ea18673b4837c7bb2ad175d6d92226068c013
FluffyGameDev <fluffygamedev@gmail.com>
2021-01-16 14:59:59 +0100

    Implemented Quicksort visualization

--- SortingVisualizer/sortingvisualizer.cpp
+++ SortingVisualizer/sortingvisualizer.cpp
@@ -2,13 +2,15 @@
 #include "olcPixelGameEngine.h"
 
 #include <numeric>
+#include <queue>
 #include <random>
 
 class SortingStrategy
 {
 public:
     virtual void PerformStep(std::vector<uint8_t>& numbers, std::pair<size_t, size_t>& comparedIndexes) = 0;
-    
+    virtual void Begin(std::vector<uint8_t>& numbers) {};
+
     void Reset()
     {
         m_IsSortingFinished = false;
@@ -67,12 +69,84 @@ private:
     bool m_DidASwap{ false };
 };
 
+class QuickSortingStrategy : public SortingStrategy
+{
+public:
+    void PerformStep(std::vector<uint8_t>& numbers, std::pair<size_t, size_t>& comparedIndexes) override
+    {
+        if (m_CurrentIndexes.first < m_CurrentIndexes.second)
+        {
+            comparedIndexes = m_CurrentIndexes;
+            if (numbers[m_CurrentIndexes.first] > numbers[m_CurrentIndexes.second])
+            {
+                std::swap(numbers[m_CurrentIndexes.first], numbers[m_CurrentIndexes.second]);
+
+                m_IsMovingLeftIndex = !m_IsMovingLeftIndex;
+            }
+
+            if (m_IsMovingLeftIndex)
+            {
+                ++m_CurrentIndexes.first;
+            }
+            else
+            {
+                --m_CurrentIndexes.second;
+            }
+        }
+        else
+        {
+            std::pair<uint32_t, uint32_t> subSectionLeft{ m_SubSections.front().first, m_CurrentIndexes.first > 0 ? m_CurrentIndexes.first - 1 : m_CurrentIndexes.first };
+            std::pair<uint32_t, uint32_t> subSectionRight{ m_CurrentIndexes.first < numbers.size() - 1 ? m_CurrentIndexes.first + 1 : m_CurrentIndexes.first, m_SubSections.front().second };
+
+            m_SubSections.pop();
+            if (subSectionLeft.first < subSectionLeft.second)
+            {
+                m_SubSections.push(subSectionLeft);
+            }
+            if (subSectionRight.first < subSectionRight.second)
+            {
+                m_SubSections.push(subSectionRight);
+            }
+
+            if (m_SubSections.empty())
+            {
+                NotifyIsFinished();
+            }
+            else
+            {
+                m_CurrentIndexes = m_SubSections.front();
+                m_IsMovingLeftIndex = true;
+            }
+        }
+    }
+
+    void Begin(std::vector<uint8_t>& numbers) override
+    {
+        m_IsMovingLeftIndex = true;
+        m_CurrentIndexes = { 0, (uint8_t)numbers.size() - 1 };
+        m_SubSections.emplace(m_CurrentIndexes);
+    }
+
+    void OnReset() override
+    {
+        while (!m_SubSections.empty())
+        {
+            m_SubSections.pop();
+        }
+    }
+
+private:
+    std::queue<std::pair<uint32_t, uint32_t>> m_SubSections;
+    std::pair<uint32_t, uint32_t> m_CurrentIndexes;
+    bool m_IsMovingLeftIndex{ true };
+};
+
 class SortingVisualizer : public olc::PixelGameEngine
 {
 public:
     SortingVisualizer()
         : m_IsSorting{ false }
-        , m_SortingStrategy{ std::make_unique<BubbleSortingStrategy>() }
+        , m_SortingStrategy{ std::make_unique<QuickSortingStrategy>() }
     {
         sAppName = "Sorting Visualizer";
     }
@@ -98,6 +172,10 @@ public:
         if (GetKey(olc::Key::SPACE).bPressed)
         {
             m_IsSorting = !m_IsSorting;
+            if (m_IsSorting)
+            {
+                m_SortingStrategy->Begin(m_Numbers);
+            }
         }
 
         std::pair<size_t, size_t> comparedIndexes{};
@@ -113,7 +191,7 @@ public:
         }
 
         Clear(olc::BLACK);
-        int32_t size = m_Numbers.size();
+        size_t size{ m_Numbers.size() };
         for (int32_t i = 0; i < size; ++i)
         {
             olc::Pixel color{ (m_IsSorting && (i == comparedIndexes.first || i == comparedIndexes.second)) ? olc::RED : olc::WHITE };
